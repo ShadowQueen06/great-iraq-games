4a16e25a119651dd224141c8fd48e0db438a5afb
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-17 02:20:26 +0200

    Add files via upload

--- games/kat.js
+++ games/kat.js
@@ -3,30 +3,28 @@ const { color } = require("../config.js");
 const questions = require("../data/kat_questions.json");
 const { addReward } = require("../utils/db.js");
 
-let lastQuestion = null;
+let lastQuestionId = null;
+
+function questionEmbed(user) {
+  let question;
+
+  do {
+    question = questions[Math.floor(Math.random() * questions.length)];
+  } while (
+    questions.length > 1 &&
+    question.id === lastQuestionId
+  );
+
+  lastQuestionId = question.id;
+
+  addReward(user, 1, 1, "kat");
+
+  return new EmbedBuilder()
+    .setColor(color)
+    .setTitle("🎭 كت")
+    .setDescription(question.question);
+}
 
 module.exports = {
-  questionEmbed(user) {
-    let question;
-
-    do {
-      question = questions[Math.floor(Math.random() * questions.length)];
-    } while (
-      lastQuestion &&
-      question.id === lastQuestion.id &&
-      questions.length > 1
-    );
-
-    lastQuestion = question;
-
-    addReward(user, 1, 1, "kat");
-
-    return new EmbedBuilder()
-      .setColor(color)
-      .setTitle("🎭 لعبة كت")
-      .setDescription(`### ${question.question}`)
-      .setFooter({
-        text: "جاوب بصراحة 😄"
-      });
-  }
+  questionEmbed
 };
