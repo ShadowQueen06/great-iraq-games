b2bcca8c201ef783fa77357f6bd2d65d9bbd9b43
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-17 02:13:42 +0200

    kat.js

--- games/kat.js
+++ games/kat.js
@@ -2,6 +2,31 @@ const { EmbedBuilder } = require("discord.js");
 const { color } = require("../config.js");
 const questions = require("../data/kat_questions.json");
 const { addReward } = require("../utils/db.js");
-let lastQuestion=null;
-function questionEmbed(user){let q;do{q=questions[Math.floor(Math.random()*questions.length)];}while(lastQuestion&&q.id===lastQuestion.id&&questions.length>1);lastQuestion=q;addReward(user,1,1,"kat");return new EmbedBuilder().setColor(color).setTitle("🎭 كت").setDescription(q.question);}
-module.exports={questionEmbed};
+
+let lastQuestion = null;
+
+module.exports = {
+  questionEmbed(user) {
+    let question;
+
+    do {
+      question = questions[Math.floor(Math.random() * questions.length)];
+    } while (
+      lastQuestion &&
+      question.id === lastQuestion.id &&
+      questions.length > 1
+    );
+
+    lastQuestion = question;
+
+    addReward(user, 1, 1, "kat");
+
+    return new EmbedBuilder()
+      .setColor(color)
+      .setTitle("🎭 لعبة كت")
+      .setDescription(`### ${question.question}`)
+      .setFooter({
+        text: "جاوب بصراحة 😄"
+      });
+  }
+};
